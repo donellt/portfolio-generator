b99b8c1fb603a89e92a1b830dc5a7b308d19ceed
Donell Work <donelltorres@yahoo.com>
2020-07-28 16:57:34 -0400

    user input added

---
+++ app.js
@@ -0,0 +1,12 @@
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+    
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+  
+printProfileData(profileDataArgs);
